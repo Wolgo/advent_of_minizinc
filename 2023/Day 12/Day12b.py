5d9605159a77b26ea5f1c27cdeca5581580d2fc0
wolgo <wiegel.erik@gmail.com>
2023-12-13 00:10:06 +0100

    Additional Strong Constraints

--- 2023/Day 12/Day12b.py
+++ 2023/Day 12/Day12b.py
@@ -2,8 +2,8 @@ from minizinc import Model, Solver, Instance
 import re
 
 # Switch between example and real instance.
-# file = open('example_input.txt', 'r')
-file = open('input.txt', 'r')
+file = open('example_input.txt', 'r')
+# file = open('input.txt', 'r')
 
 # Setup Minizinc
 model = Model("Day12.mzn")
@@ -41,14 +41,10 @@ for line in lines:
         elif value == '?':
             measurements[idx] = 3
     measurements5 = measurements.copy()
-    power = False
-    # If the last and first number is 1, we don't need to calculate all possiblities, we can just power 5
-    if measurements5[len(measurements5) - 1] != 1 or measurements5[1] != 1:
-        for j in range(1, 5):
-            measurements5.extend([3])
-            measurements5.extend(measurements)
-    else:
-        power = True
+
+    for j in range(1, 5):
+        measurements5.extend([3])
+        measurements5.extend(measurements)
 
     # Part B: Remove any consecutive working machines to limit search space.
     new_measures = []
@@ -58,7 +54,6 @@ for line in lines:
                 new_measures.append(value)
         else:
             new_measures.append(value)
-    print(new_measures)
 
     instance["length_measurements"] = len(new_measures)
     instance["measurements"] = list(map(int, new_measures))
@@ -66,10 +61,7 @@ for line in lines:
     # find all Solutions instead of only 1
     result = instance.solve(all_solutions=True)
     # Keep track of the count
-    if power:
-        count += pow(len(result.solution), 5)
-    else:
-        count += len(result.solution)
+    count += len(result.solution)
     # Show Progress to ensure we do know when we should improve performance
     i += 1
     print("solved " + str(i) + " out of " + str(all))
